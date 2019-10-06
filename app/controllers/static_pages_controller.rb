03d635acc4bffd4c5808ef02e532c12a00628f7a
Ben Sommerville <bs@Bens-MacBook-Air.local>
2019-10-05 22:03:02 -0400

    phase 1

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def inventory
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/inventory'
+  get 'static_pages/contact'
+  get 'static_pages/index'
+  resources :static_pages
+  resources :microposts
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
 end
